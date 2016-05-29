49a33b700fe96e255d2202886c63e202d3b1aa90
benbal <ballubenjamin@gmail.com>
2016-05-29 17:33:12 +0200

    version 0.5

--- main.cpp
+++ main.cpp
@@ -1,35 +1,39 @@
 #include "mbed.h"
-
-DigitalOut LedV(p21),LedR(p22), Led1(LED1),Led2(LED2),Led3(LED3),Led4(LED4);
-DigitalIn BP(p5);
-int main () {
-    int m = 0;
-    bool l = true;
-    while(1) {
-        if (BP==1){     
-                if (l==true){
-                    LedV=0,LedR=0,Led1=0,Led2=0,Led3=0,Led4=0;
-                    m+=1;
-                    if (m==1){
-                        LedV=1,Led1=1;
-                    }
-                    if (m==2){
-                        LedR=1,Led2=1;
-                    }
-                    if (m==3){
-                        LedR=1,LedV=1,Led3=1;
-                        }
-                    if (m>3){
-                        Led4=1;
-                        m=0;
-                    }
-                    l = false;
-                }  
-                wait (0.01);              
+#include "Serial.h"
+//#include "buffered_serial.h"
+DigitalOut LedV(p21),LedR(p22),Led1(LED1)/*,Led2(LED2),Led3(LED3),Led4(LED4)*/;
+//InterruptIn button(p5);
+Serial pc(USBTX, USBRX);
+//Buffer pcbuffer(USBTX, USBRX);
+//unsigned char c [1]={};
+ int vitesse=9600;
+ //int test[] = { 1 , 2 ,3  ,4  , 5 };
+ bool b=false;
+void a (){
+    //pc.putc(pc.getc());
+    int op=pc.getc();
+    if(op==77){
+        b=true;
+        Led1=1;
         }
-        if (BP==0){
-            l=true ;
+    if(op==65){
+        b=false;
+        Led1=0;
         }
-        
-   }
+} 
+int main() {
+     //pc.read(c,1,, &a, 77);
+    pc.baud(vitesse);
+    pc.attach(&a);
+    while(1){
+        if (b){
+            LedV = 1;
+            wait(0.05);
+            LedV=0;
+            LedR=1;
+            wait (0.2);
+            LedR=0;
+           //pc.putc(c[1]);
+        }
+    }
 }
